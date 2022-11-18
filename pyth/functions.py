b9a5419554296de0d54a67a4a030903a3b2893e6
Robert Castro <76708448+Windows-CPP@users.noreply.github.com>
2022-11-17 22:55:33 -0700

    Added autoImageManifest()
    Added a func to automatically detect num of images in a dir and add them to a returned dictionary, assuming that images are in sequential order from 0 to inf.

--- pyth/functions.py
+++ pyth/functions.py
@@ -1,5 +1,24 @@
+import os
 from os import system
 
 def cls():
     'Clears the CLI.'
     system("cls")
+
+def autoImageManifeset(location):
+    'Automatically scans the number and names of all images inside of the specefied directory, and puts them into a returned array.\n\nUsage: autoImageManifest(location)'
+    numFiles = 0
+    fileLocAr = {}
+
+    # Scanning for no of files in directory using os.listdir
+    for path in os.listdir(location):
+        if os.path.isfile(os.path.join(location, path)):
+            numFiles += 1
+    
+    # Getting file names and types & adding to array
+    for i in range(numFiles):
+        tempString = "./images/" + str(i) + ".jpg"
+        fileLocAr[i] = tempString
+    
+    # Returning file locations in dictionary
+    return fileLocAr
