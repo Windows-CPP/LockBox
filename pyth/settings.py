d5ccae5d46403317f8250f5be39394b3b98486a3
Robert Castro <76708448+Windows-CPP@users.noreply.github.com>
2023-10-19 11:58:42 -0700

    continued work on classHouse

--- pyth/settings.py
+++
@@ -1,9 +0,0 @@
-genInf = {
-    "build":"wint-stand",
-    "version":"v0.6.5-b"
-}
-
-settings = {
-    "enclevel":2, # Encryption level. Multiples of 128- Use int 1 for 128, 2 for 256, etc. (referencing # of encrypted chars / char) || 2 by default lol
-    "igf":2
-}
